d9e40a8492778fdc3d79863f462311f3a939272d
Roman Lisagor <rlisagor@gmail.com>
2010-01-18 15:57:50 -0800

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = "pynetlinux",
+    version = "1.0",
+    description = "Linux networking interface library for Python",
+    author = "Roman Lisagor",
+    author_email = "rlisagor+pynetlinux@gmail.com",
+    url = "http://github.com/rlisagor/pynetlinux",
+    packages = ["pynetlinux"]
+)
